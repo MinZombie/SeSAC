06d3604c685db6e7a154f9f9a1d8523e72840ef7
MinZombie <sg.min0410@gmail.com>
2021-10-10 17:49:49 +0900

    [기능추가] Alert 기능 추가, TextField 조건 추가, 라이브러리 추가 모든 TextField에 text가 필요하고, 한 곳이라도 비어 있다면 alert로 내용을 채워야 한다고 알려준다. IQKeyboardManagerSwift 라이브러리를 활용해서 키보드가 TextField를 가리는 문제 해결
    -

--- DrinkWater/DrinkWater/ProfileViewController.swift
+++ DrinkWater/DrinkWater/ProfileViewController.swift
@@ -38,8 +38,12 @@ class ProfileViewController: UIViewController {
     
     private func setUpView() {
         view.backgroundColor = .systemGreen
-        
+    
         navigationController?.navigationBar.tintColor = .white
+        
+        nicknameTextField.delegate = self
+        heightTextField.delegate = self
+        weightTextField.delegate = self
         
         saveBarButton.target = self
         saveBarButton.action = #selector(didTapSaveBarButton(_:))
@@ -58,25 +62,63 @@ class ProfileViewController: UIViewController {
         
         nicknameTextField.textColor = .label
         nicknameTextField.font = .systemFont(ofSize: 23)
+        nicknameTextField.returnKeyType = .next
         
         heightTextField.textColor = .label
         heightTextField.font = .systemFont(ofSize: 23)
+        heightTextField.keyboardType = .numberPad
         
         weightTextField.textColor = .label
         weightTextField.font = .systemFont(ofSize: 23)
+        weightTextField.keyboardType = .numberPad
     }
                               
     @objc func didTapSaveBarButton(_ sender: UIBarButtonItem) {
-        guard let nickname = nicknameTextField.text, let height = heightTextField.text, let weight = weightTextField.text else {
+        save()
+        
+
+    }
+    
+    private func save() {
+        guard let nickname = nicknameTextField.text, let height = heightTextField.text, let weight = weightTextField.text,
+              !nickname.trimmingCharacters(in: .whitespaces).isEmpty, !height.trimmingCharacters(in: .whitespaces).isEmpty,
+              !weight.trimmingCharacters(in: .whitespaces).isEmpty
+        else {
+            alert(title: "모든 정보를 입력해주세요.", message: nil, buttonTitle: "확인")
             return
         }
+        
+        if nickname.count > 10 {
+            alert(title: "닉네임은 10자 이하로 정해주세요.", message: nil, buttonTitle: "확인")
+        }
+        
         if let encoded = try? JSONEncoder().encode(User(nickname: nickname, height: Float(height) ?? 0, weight: Float(weight) ?? 0, state: .hasInfo)) {
             UserDefaults.standard.setValue(encoded, forKey: Constants.UserDefaultsKeys.userDefault)
         }
 
         self.navigationController?.popViewController(animated: true)
-        
+    }
+    
+    private func alert (title: String, message: String?, buttonTitle: String) {
+        self.view.endEditing(true)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: buttonTitle, style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+}
 
+extension ProfileViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // 닉네임 텍스트필드에서 next버튼 누르면 다음 텍스트필드로 이동
+        if textField == nicknameTextField {
+            heightTextField.becomeFirstResponder()
+        }
+        
+        return true
     }
 
 }
