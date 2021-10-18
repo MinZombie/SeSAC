3800ae9fc67c469d32659577513dd83f75cba4ea
MinZombie <sg.min0410@gmail.com>
2021-10-18 21:12:27 +0900

    구조체, 상수 이름변경

--- TrendMedia/TrendMedia/Other/Data.swift
+++ TrendMedia/TrendMedia/Other/Data.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-let tvShow: [TvShow] = [
+let dummydata: [TvShow] = [
     
     TvShow(title: "Squid Game", releaseDate: "09/17/2021",genre: "Mystery",region: "South Korea", overview: "Hundreds of cash-strapped players accept a strange invitation to compete in children's games. Inside, a tempting prize awaits — with deadly high stakes.", rate: 8.3, starring: "Lee Jung-jae, Park Hae-soo, Wi Ha-jun, Heo Sung-tae, Kim Joo-ryoung, Jung Ho-yeon, Lee You-mi",backdropImage:"https://www.themoviedb.org/t/p/original/oaGvjB0DvdhXhOAuADfHb261ZHa.jpg"),
     
