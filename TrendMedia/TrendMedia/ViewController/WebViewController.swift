3e5de7b2031a81d7e80a8df3f87cad31614d6c41
MinZombie <sg.min0410@gmail.com>
2021-10-20 15:56:54 +0900

    [기능구현] WebViewController 추가 및 웹뷰 기능 구현

--- TrendMedia/TrendMedia/ViewController/WebViewController.swift
+++ TrendMedia/TrendMedia/ViewController/WebViewController.swift
@@ -6,27 +6,63 @@
 //
 
 import UIKit
+import WebKit
 
 class WebViewController: UIViewController {
     
     var tvShowTitle: String?
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var reloadButton: UIBarButtonItem!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var fowardButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("WebViewController did load")
-        title  = tvShowTitle
+        
+        searchBar.delegate = self
+        setUpButton()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUpButton() {
+        reloadButton.action = #selector(didTapReloadButton)
+        reloadButton.target = self
+        
+        backButton.action = #selector(didTapBackButton)
+        backButton.target = self
+        
+        fowardButton.action = #selector(didTapFowardButton)
+        fowardButton.target = self
     }
-    */
+    
+    @objc func didTapReloadButton() {
+        webView.reload()
+    }
+    
+    @objc func didTapBackButton() {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    @objc func didTapFowardButton() {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+}
 
+extension WebViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let url = URL(string: searchBar.text ?? "") else {
+            print("Invalid URL")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
 }
