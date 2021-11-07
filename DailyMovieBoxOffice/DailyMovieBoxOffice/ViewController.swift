68c87a34539c2109380960f0133f119d328693d6
MinZombie <sg.min0410@gmail.com>
2021-11-07 14:44:07 +0900

    [기능추가] 날짜 검색하면 해당 날짜 박스오피스 보여주는 기능

--- DailyMovieBoxOffice/DailyMovieBoxOffice/ViewController.swift
+++ DailyMovieBoxOffice/DailyMovieBoxOffice/ViewController.swift
@@ -15,6 +15,7 @@ import RealmSwift
 class ViewController: UIViewController {
     
     @IBOutlet weak var movieTableView: UITableView!
+    @IBOutlet weak var boxOfficeDateSearchbar: UISearchBar!
     
     let realm = try! Realm()
     
@@ -28,25 +29,48 @@ class ViewController: UIViewController {
         movieTableView.dataSource = self
         movieTableView.delegate = self
         
+        boxOfficeDateSearchbar.delegate = self
+        
         print("File URL: \(realm.configuration.fileURL!)")
         
         checkUserData()
-        
-        
+        configureSearchbar()
     }
     
-    private func requestWhenUserNoData() {
-        Manager.shared.request(date: getYesterdayDate()) { response in
-            let result = response.boxOfficeResult.dailyBoxOfficeList
+    private func searchDate(date: String) {
+        
+        let filteredMovie = realm.objects(DateList.self).filter("targetDt CONTAINS '\(date)'")
+        
+        
+        if filteredMovie.isEmpty {
+            // 데이터가 비어 있으면 그 날짜 영화 정보 가져오기
+            requestWhenUserNoData(date: date)
             
+        } else {
+            // 있으니까 realm에서 저장돼있는 데이터 가져와서 보여주기
+            currentMovies = filteredMovie.first!.movies
+
+            movieTableView.reloadData()
+        }
+    }
+    
+    // date 파라미터 없애기
+    private func requestWhenUserNoData(date: String) {
+        Manager.shared.request(date: date) { response in
+            let result = response.boxOfficeResult.dailyBoxOfficeList
+            let temp =  List<Movie>()
+            
+            // 현재 테이블뷰에 보여줄 영화 저장하는 부분
             result.forEach {
-                let movie = Movie(movieNm: $0.movieNm, id: self.getYesterdayDate())
-                self.currentMovies.append(movie)
+                let movie = Movie(movieNm: $0.movieNm, id: date)
+                temp.append(movie)
             }
+            
+            self.currentMovies = temp
             
             try! self.realm.write {
                 self.realm.add(self.currentMovies)
-                self.realm.add(DateList(targetDt: self.getYesterdayDate(), movies: self.currentMovies))
+                self.realm.add(DateList(targetDt: date, movies: self.currentMovies))
             }
             
             self.movieTableView.reloadData()
@@ -55,25 +79,26 @@ class ViewController: UIViewController {
     
     private func checkUserData() {
         let tasks = realm.objects(DateList.self).sorted(byKeyPath: "targetDt", ascending: false)
-
+        
         guard let latestDate = tasks.first else {
             // 데이터 없음
-            requestWhenUserNoData()
+            requestWhenUserNoData(date: getYesterdayDate())
             print("데이터 없다")
             return
         }
         
         if latestDate.targetDt != getYesterdayDate() {
             // 최근 데이터 없음
-            requestWhenUserNoData()
+            requestWhenUserNoData(date: getYesterdayDate())
             print("최근 데이터 없음")
         } else {
+            // realm에 데이터 있어서 가져와서 보여주기
             print("데이터 있음")
             currentMovies = tasks.first!.movies
             movieTableView.reloadData()
         }
     }
-
+    
     private func getYesterdayDate() -> String {
         let yesterday = Date() - 86400
         
@@ -84,7 +109,9 @@ class ViewController: UIViewController {
         
     }
     
-    
+    private func configureSearchbar() {
+        boxOfficeDateSearchbar.showsCancelButton = true
+    }
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
@@ -99,4 +126,28 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text?.trimmingCharacters(in: .whitespaces), text.count == 8 else {
+            // Alert 보여주기
+            print("글자수 8자리 아님")
+            return
+        }
+        
+        searchDate(date: text)
+    }
+    
+    // 취소 버튼 눌렀을 때 키보드 내리기
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        boxOfficeDateSearchbar.text = ""
+        searchBar.resignFirstResponder()
+    }
+    
 }
